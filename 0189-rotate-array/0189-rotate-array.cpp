f3fb3d64461fb9501f540ac507eab7cf4a8e232c
KAPIL AHMED <kapil25ei10066@gmail.com>
2026-09-01 16:14:58 +0530

    Time: 7 ms (11.97%), Space: 263.4 MB (21.47%) - LeetHub

---
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int n=nums.size();
+        if(n==1){
+            return;
+        }
+     reverse(nums.begin(),nums.end());
+    
+     k=k%(n); 
+     reverse(nums.begin(),nums.begin()+k);
+     reverse(nums.begin()+k,nums.end());
+     return;
+    }
+};
