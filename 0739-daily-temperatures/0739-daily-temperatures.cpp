025981e3eed2dd9da71221370d0d8fea0befb75d
KAPIL AHMED <kapil25ei10066@gmail.com>
2026-09-07 16:57:03 +0530

    Time: 24 ms (45.05%), Space: 102.7 MB (91.36%) - LeetHub

---
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temp) {
+       
+        int n=temp.size();
+        stack<int>st;
+        vector<int>res(n);
+        res[n-1]=0;
+        st.push(n-1);
+        for(int i=n-2;i>=0;i--){
+            while(!st.empty() && temp[i]>=temp[st.top()]){
+                st.pop();
+            }
+            if(st.empty()){
+                res[i]=0;
+                st.push(i);
+            }
+            else{
+                res[i]=st.top()-i;
+                 st.push(i);
+            }
+        }
+        return res;
+    }
+};
